1ebe08e1b4af919b718146cdd05e51d504db6e16
Nathan Barta <71276691+NathanBarta@users.noreply.github.com>
2024-01-22 20:30:45 -0500

    Initial Commit

---
+++ CSCWorkshop/CSCWorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSCWorkshopApp.swift
+//  CSCWorkshop
+//
+//  Created by Nathan Barta on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct CSCWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
